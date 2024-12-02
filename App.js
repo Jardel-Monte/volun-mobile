41946ced2fc91a12c3d5645796632b1daa82b633
andersonbs96 <andersonsantos6991@gmail.com>
2024-12-02 11:52:55 -0300

    Atualizando os componentes de Dados e Informação Pessoal

--- App.js
+++ App.js
@@ -38,7 +38,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Login'>
         <Stack.Screen
-          name='Login'
+          name='LoginScreen'
           component={LoginScreen}
           options={{ headerShown: false }}
         />
