f7c505885caaf8e979d00352ef7e1599c406fc43
unknown <nicolasgabrielbustamantelopes@gmail.com>
2024-12-02 11:17:15 -0300

    tabs

--- screens/MyTabs.js
+++ screens/MyTabs.js
@@ -12,63 +12,69 @@ const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
+    <View style={styles.tabBarContainer}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-          switch (route.name) {
-            case 'Início':
-              iconName = 'home';
-              break;
-            case 'Artigos':
-              iconName = 'article';
-              break;
-            case 'Notificações':
-              iconName = 'notifications';
-              break;
-            case 'Buscar':
-              iconName = 'search';
-              break;
-            case 'Perfil':
-              iconName = 'person';
-              break;
-            default:
-              iconName = 'circle';
-              break;
-          }
+            switch (route.name) {
+              case 'Início':
+                iconName = 'home';
+                break;
+              case 'Artigos':
+                iconName = 'article';
+                break;
+              case 'Notificações':
+                iconName = 'notifications';
+                break;
+              case 'Buscar':
+                iconName = 'search';
+                break;
+              case 'Perfil':
+                iconName = 'person';
+                break;
+              default:
+                iconName = 'circle';
+                break;
+            }
 
-          return (
-            <View style={[styles.iconContainer, focused && styles.activeIconContainer]}>
-              <Icon name={iconName} size={size} color={color} />
-            </View>
-          );
-        },
-        tabBarActiveTintColor: 'white',
-        tabBarInactiveTintColor: 'tomato',
-        tabBarStyle: {
-          height: 64,
-          paddingBottom: 0,
-          paddingTop: 0,
-          marginBottom: 5,
-          backgroundColor: 'white', // Set the tab bar background color to white
-          borderRadius: 64,
-          marginLeft: 3,
-          marginRight: 3,
-        },
-        tabBarLabel: () => null, // Hide the default tab label
-      })}
-    >
-      <Tab.Screen name="Início" component={HomeScreen} />
-      <Tab.Screen name="Artigos" component={OrgScreen} />
-      <Tab.Screen name="Notificações" component={NotificacoesScreen} />
-      <Tab.Screen name="Buscar" component={SearchScreen} />
-      <Tab.Screen name="Perfil" component={PerfilScreen} />
-    </Tab.Navigator>
+            return (
+              <View style={[styles.iconContainer, focused && styles.activeIconContainer]}>
+                <Icon name={iconName} size={size} color={color} />
+              </View>
+            );
+          },
+          tabBarActiveTintColor: 'white',
+          tabBarInactiveTintColor: 'tomato',
+          tabBarStyle: {
+            height: 64,
+            paddingBottom: 0,
+            paddingTop: 0,
+            marginBottom: 5,
+            backgroundColor: '#FBFBFE',
+            borderRadius: 64,
+            marginLeft: 3,
+            marginRight: 3,
+          },
+          tabBarLabel: () => null, // Hide the default tab label
+        })}
+      >
+        <Tab.Screen name="Início" component={HomeScreen} />
+        <Tab.Screen name="Artigos" component={OrgScreen} />
+        <Tab.Screen name="Notificações" component={NotificacoesScreen} />
+        <Tab.Screen name="Buscar" component={SearchScreen} />
+        <Tab.Screen name="Perfil" component={PerfilScreen} />
+      </Tab.Navigator>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  tabBarContainer: {
+    flex: 1,
+    backgroundColor: '#FBFBFB', // Set the background color to #FFFFFF
+  },
   iconContainer: {
     alignItems: 'center',
     justifyContent: 'center',
@@ -76,7 +82,7 @@ const styles = StyleSheet.create({
     padding: 6,
   },
   activeIconContainer: {
-    backgroundColor: "rgba(57, 66, 134, 0.88)",
+    backgroundColor: "rgba(57, 67, 180, 0.88)",
   },
 });
 
